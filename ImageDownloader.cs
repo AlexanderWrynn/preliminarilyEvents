aa7d38ab3aa98d75380bbc8a455395aec7e4f4bb
Alex Z <can4you@yandex.ru>
2021-11-14 19:22:59 +0300

    Добавьте файлы проекта.

---
+++ ImageDownloader.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Net;
+
+namespace preliminarilyEvents
+{
+    class ImageDownloader
+    {
+        public event Action<string> ImageStarted;
+        public event Action<string> ImageCompleted;
+
+        public void Download(string remoteUri, string fileName)
+        {
+            var myWebClient = new WebClient();
+            ImageStarted?.Invoke("Скачивание файла началось");
+            Console.WriteLine("Качаю \"{0}\" из \"{1}\" .......\n\n", fileName, remoteUri);
+            myWebClient.DownloadFile(remoteUri, fileName);
+            ImageCompleted?.Invoke("Скачивание файла закончилось");
+            Console.WriteLine("Успешно скачал \"{0}\" из \"{1}\"", fileName, remoteUri);
+
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace preliminarilyEvents
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string remoteUri = "https://effigis.com/wp-content/uploads/2015/02/Iunctus_SPOT5_5m_8bit_RGB_DRA_torngat_mountains_national_park_8bits_1.jpg";
+            string fileName = "bigimage.jpg";
+
+            var imageDownloader = new ImageDownloader();
+            imageDownloader.ImageStarted += message => Console.WriteLine(message);
+            imageDownloader.Download(remoteUri, fileName);
+            imageDownloader.ImageCompleted += message => Console.WriteLine(message);
+            Console.WriteLine("Нажмите любую клавишу для выхода");
+            Console.ReadKey();
+
+        }
+
+    }
+}
